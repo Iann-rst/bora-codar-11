3b7f57519e576844f9eacd99ff06db444c2ba5c8
Iann Rodrigues <iannrst11@gmail.com>
2023-05-08 12:16:30 -0300

    feat: Configurando as cores do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: '#F8FAFC',
+          100: '#F1F5F9',
+          200: '#E2E8F0',
+          300: '#CBD5E1',
+          400: '#94A3B8',
+          500: '#64748B',
+          600: '#475569',
+          700: '#334155',
+          800: '#1E293B',
+          900: '#0F172A',
+
+        },
+
+        purple: {
+         500: '#7C3AED' 
+        }
+      }
+    },
   },
   plugins: [],
 }
